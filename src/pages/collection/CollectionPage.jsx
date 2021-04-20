5fae7b592e3691ba4a221c5e6bd348ec1922b31a
Alex Hu <xindi_hu@yahoo.com>
2021-04-20 13:59:02 -0500

    nested collection page url inside shop page

---
+++ src/pages/collection/CollectionPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selector";
+
+import "./collection.scss";
+
+const CollectionPage = ({ collection }) => {
+  return (
+    <div className="collection-page">
+      <h2>{collection.title}</h2>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
