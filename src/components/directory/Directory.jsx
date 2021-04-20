c1344435950cdedf3dea4294b49f1e195c0b3f0e
Alex Hu <xindi_hu@yahoo.com>
2021-04-20 11:42:26 -0500

    moved state to redux

--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -1,63 +1,20 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectDirectorySections } from "../../redux/directory/directory.selector";
 import MenuItem from "../menu-item/MenuItem";
 import "./directory.scss";
 
-class Directory extends React.Component {
-  constructor() {
-    super();
+const Directory = ({ sections }) => (
+  <div className="directory-menu">
+    {sections.map(({ id, ...otherSectionProps }) => {
+      return <MenuItem key={id} {...otherSectionProps} />;
+    })}
+  </div>
+);
 
-    this.state = {
-      section: [
-        {
-          title: "hats",
-          imgURL:
-            "https://images.unsplash.com/photo-1533055640609-24b498dfd74c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80",
-          id: 1,
-          linkURL: "hats",
-        },
-        {
-          title: "jackets",
-          imgURL:
-            "https://images.unsplash.com/photo-1559551409-dadc959f76b8?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8amFja2V0c3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-          id: 2,
-          linkURL: "",
-        },
-        {
-          title: "sneakers",
-          imgURL:
-            "https://images.unsplash.com/photo-1612902376491-7a8a99b424e8?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fHNuZWFrZXJzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-          id: 3,
-          linkURL: "",
-        },
-        {
-          title: "womens",
-          imgURL:
-            "https://images.unsplash.com/photo-1498032087969-fd7d126f9d0b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-          id: 4,
-          size: "large",
-          linkURL: "",
-        },
-        {
-          title: "mens",
-          imgURL:
-            "https://images.unsplash.com/photo-1505022610485-0249ba5b3675?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-          id: 5,
-          size: "large",
-          linkURL: "",
-        },
-      ],
-    };
-  }
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
 
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.section.map(({ id, ...otherSectionProps }) => {
-          return <MenuItem key={id} {...otherSectionProps} />;
-        })}
-      </div>
-    );
-  }
-}
-
-export default Directory;
+export default connect(mapStateToProps)(Directory);
---
+++ src/redux/directory/directory.reducer.js
@@ -0,0 +1,50 @@
+const INITIAL_STATE = {
+  sections: [
+    {
+      title: "hats",
+      imgURL:
+        "https://images.unsplash.com/photo-1533055640609-24b498dfd74c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80",
+      id: 1,
+      linkURL: "hats",
+    },
+    {
+      title: "jackets",
+      imgURL:
+        "https://images.unsplash.com/photo-1559551409-dadc959f76b8?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8amFja2V0c3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      id: 2,
+      linkURL: "",
+    },
+    {
+      title: "sneakers",
+      imgURL:
+        "https://images.unsplash.com/photo-1612902376491-7a8a99b424e8?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fHNuZWFrZXJzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      id: 3,
+      linkURL: "",
+    },
+    {
+      title: "womens",
+      imgURL:
+        "https://images.unsplash.com/photo-1498032087969-fd7d126f9d0b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+      id: 4,
+      size: "large",
+      linkURL: "",
+    },
+    {
+      title: "mens",
+      imgURL:
+        "https://images.unsplash.com/photo-1505022610485-0249ba5b3675?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+      id: 5,
+      size: "large",
+      linkURL: "",
+    },
+  ],
+};
+
+const directoryReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default directoryReducer;
