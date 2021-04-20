042373d1ebf994f38b6cfc1a00c3622ddfcdfcb8
Alex Hu <xindi_hu@yahoo.com>
2021-04-20 12:04:49 -0500

    moved shop page state to redux and convert shop page to function component

--- src/pages/shop-page/ShopPage.jsx
+++ src/pages/shop-page/ShopPage.jsx
@@ -1,23 +1,19 @@
-import React, { Component } from "react";
-import SHOP_DATA from "../../assets/shop_data";
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import PreviewCollection from "../../components/preview-collection/PreviewCollection";
+import { selectCollections } from "../../redux/shop/shop.selector";
 
-export default class ShopPage extends Component {
-  constructor(props) {
-    super(props);
+const ShopPage = ({ collections }) => (
+  <div className="shop=page">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <PreviewCollection key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop=page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <PreviewCollection key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(ShopPage);
