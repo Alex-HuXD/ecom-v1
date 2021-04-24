3971b04693ed5751a93a1d7d86c5097a59fdad04
Alex Hu <xindi_hu@yahoo.com>
2021-04-24 13:45:58 -0500

    removed code that moves shop-data to backend

--- src/App.js
+++ src/App.js
@@ -10,20 +10,15 @@ import ShopPage from "./pages/shop-page/ShopPage";
 import SignInAndSignUpPage from "./pages/sign-in&up/SignInAndSignUpPage";
 import CheckoutPage from "./pages/checkout-page/CheckoutPage";
 
-import {
-  auth,
-  createUserProfileDoc,
-  addCollectionAndDocs,
-} from "./assets/firebase/firebase.utils";
+import { auth, createUserProfileDoc } from "./assets/firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selector";
-import { selectCollectionsForPreview } from "./redux/shop/shop.selector";
 
 class App extends React.Component {
   unsubscribedFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser, collectionsArr } = this.props;
+    const { setCurrentUser } = this.props;
 
     this.unsubscribedFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
@@ -39,10 +34,6 @@ class App extends React.Component {
         });
       }
       setCurrentUser(userAuth);
-      addCollectionAndDocs(
-        "collections",
-        collectionsArr.map(({ title, items }) => ({ title, items }))
-      );
     });
   }
 
@@ -77,7 +68,6 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
-  collectionsArr: selectCollectionsForPreview,
 });
 
 const mapDispatchToProps = (dispatch) => ({
